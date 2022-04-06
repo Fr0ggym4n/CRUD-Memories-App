6d2928586768ec1f9bf0b488fb1466ee4affcaaa
Fr0ggym4n <charlescampet03@gmail.com>
2022-04-06 20:07:06 +0200

    add authentication

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,16 +7,18 @@ import {
     deletePost,
     likePost
 } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 // CRUD routes for posts
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
 
 // Routes for like button
-router.patch('/:id/likePost', likePost);
+router.patch('/:id/likePost', auth, likePost);
+
 
 export default router;
---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { signin, signup } from '../controllers/user.js';
+
+const router = express.Router();
+
+router.post('/signin', signin); // details from the form.
+router.post('/signup', signup); // details from the form.
+
+
+export default router;
